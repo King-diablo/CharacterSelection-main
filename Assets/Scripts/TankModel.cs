d41efb59bd63a22d93c5e1e9c82a2f1e248e4ce7
Chikeluba Obiora-Eze <chikelubaobioraeze@gmail.com>
2022-05-04 14:29:38 +0100

    Created player movement using MVC and implemented camera follow

--- Assets/Scripts/TankModel.cs
+++ Assets/Scripts/TankModel.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class TankModel
 {
     private TankController tankController;
+    public float moveSpeed;
+    public float rotationSpeed;
 
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
+    }
+
+    public TankModel(float _moveSpeed, float _rotationSpeed)
+    {
+        moveSpeed = _moveSpeed;
+        rotationSpeed = _rotationSpeed;
     }
 }
